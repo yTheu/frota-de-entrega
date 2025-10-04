7d0038e53e456b8f972331e9a2d0e6ca00942e51
lipedro09 <lipe88070@gmail.com>
2025-10-04 18:25:52 -0300

    agora vai!!

--- gerenciar_frota/app/forms.py
+++ gerenciar_frota/app/forms.py
@@ -1,15 +1,15 @@
 from django import forms
-from .models import Veiculo, Motorista
+from .models import Veiculo, Motorista, Entrega, Manutencao, Abastecimento, Coordenada
 
 class VeiculoForm(forms.ModelForm):
     class Meta:
         model = Veiculo
-        fields = ['placa', 'modelo', 'km', 'autonomia', 'ultimaManutencao']
+        fields = ['placa', 'modelo', 'km', 'autonomia', 'ultimaManutencao', 'disponivel']
 
 class MotoristaForm(forms.ModelForm):
     class Meta:
         model = Motorista
-        fields = ['cpf', 'nome', 'num_cnh', 'veiculoAtual']
+        fields = ['cpf', 'nome', 'num_cnh', 'veiculoAtual', 'disponivel']
 
 class EntregaForm(forms.ModelForm):
     class Meta:
@@ -19,4 +19,15 @@ class EntregaForm(forms.ModelForm):
 class ManutencaoForm(forms.ModelForm):
     class Meta:
         model = Manutencao
-        fields = ['veiculo', 'tipo', 'descricao', 'data', 'custo']
+        fields = ['veiculo', 'tipo', 'descricao', 'data', 'custo', 'status']
+
+class AbastecimentoForm(forms.ModelForm):
+    class Meta:
+        model = Abastecimento
+        fields = ['litros', 'custo', 'dataAbastecimento', 'veiculo']
+
+class CoordenadaForm(forms.ModelForm):
+    class Meta:
+        model = Coordenada
+        fields = ['latitude', 'longitude']
+
--- gerenciar_frota/app/models.py
+++ gerenciar_frota/app/models.py
@@ -6,7 +6,9 @@ class Veiculo(models.Model):
     km = models.PositiveIntegerField()
     autonomia = models.DecimalField(max_digits=5, decimal_places=2)
     ultimaManutencao = models.DateField()
-
+    disponivel = models.BooleanField(default=True)
+    
+   
     def __str__(self):
         return f"{self.modelo} ({self.placa})"
 
@@ -16,6 +18,7 @@ class Motorista(models.Model):
     nome = models.CharField(max_length=100)
     num_cnh = models.CharField(max_length=14)
     veiculoAtual = models.ForeignKey('Veiculo', on_delete=models.SET_NULL, null=True, blank=True)
+    disponivel = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.nome} ({self.cpf})"
@@ -25,12 +28,19 @@ class Manutencao(models.Model):
         ("PREVENTIVA", 'Preventiva'),
         ("CORRETIVA", 'Corretiva'),
     ]
+    STATUS_MANUTENCAO = [
+        ("PENDENTE", "Pendente"),
+        ("CONCLUIDA", "Concluída"),
+    ] 
 
     veiculo = models.ForeignKey('Veiculo', on_delete=models.CASCADE)
     tipo = models.CharField(max_length=10, choices=TIPO_MANUTENCAO)
     descricao = models.TextField()
     data = models.DateField()
     custo = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, default="PENDENTE", choices= STATUS_MANUTENCAO)
+    
+   
 
     def __str__(self):
         return f"{self.get_tipo_display()} - {self.veiculo.modelo} ({self.data})"
@@ -66,3 +76,4 @@ class Entrega(models.Model):
         default="PENDENTE"
     )
     veiculo = models.ForeignKey('Veiculo', on_delete=models.SET_NULL, null=True, blank=True)
+   
